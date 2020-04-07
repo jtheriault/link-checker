cf4c8ad5d6803671b8ce7e3d41b056eabb5fee21
Joe Theriault <joe.theriault@meltwater.com>
2020-04-07 12:33:04 -0400

    Resolve shellcheck warnings

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -l
+#!/bin/sh
 set -uo pipefail
 
 LINKCHECKER_OUTPUT_DIR=${LINKCHECKER_OUTPUT_DIR:="link-checker"}
@@ -8,16 +8,19 @@ LINKCHECKER_OUTPUT_FILENAME=${LINKCHECKER_OUTPUT_FILENAME:="out.md"}
 mkdir -p /tmp/link-checker
 
 # Execute Liche
-liche $* >/tmp/link-checker/out 2>&1
+liche "$@" >/tmp/link-checker/out 2>&1
 exit_code=$?
 
 # If link errors were found output a report to the designated directory
 if [ $exit_code -eq 1 ]; then
     mkdir -p $LINKCHECKER_OUTPUT_DIR
-    echo -e '### Link Checker\nErrors were reported while checking the connectivity of links.\n```' \
-        >$LINKCHECKER_OUTPUT_DIR/$LINKCHECKER_OUTPUT_FILENAME
-    cat /tmp/link-checker/out >>$LINKCHECKER_OUTPUT_DIR/$LINKCHECKER_OUTPUT_FILENAME
-    echo '```' >>$LINKCHECKER_OUTPUT_DIR/$LINKCHECKER_OUTPUT_FILENAME
+    cat << EOF > $LINKCHECKER_OUTPUT_DIR/$LINKCHECKER_OUTPUT_FILENAME
+### Link Checker:
+Errors were reported while checking the connectivity of links.
+\`\`\`
+$(cat /tmp/link-checker/out)
+\`\`\`
+EOF
     echo "Link checker output file: $LINKCHECKER_OUTPUT_DIR/$LINKCHECKER_OUTPUT_FILENAME"
 fi
 
